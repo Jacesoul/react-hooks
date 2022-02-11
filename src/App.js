e20299bfc1d50f6e61470a973e85fac1e92c1f20
Jacesoul <75877363+Jacesoul@users.noreply.github.com>
2022-02-11 14:35:10 +0900

    useClick

--- src/App.js
+++ src/App.js
@@ -38,13 +38,30 @@ export const useTitle = (initialTitle) => {
   return setTitle;
 };
 
+export const useClick = (onClick) => {
+  // if (typeof onClick !== "function") {
+  //   return;
+  // }
+  const element = useRef();
+  useEffect(() => {
+    if (element.current) {
+      element.current.addEventListener("click", onClick);
+    }
+    return () => {
+      if (element.current) {
+        element.current.removeEventListener("click", onClick);
+      }
+    };
+  }, []);
+  return element;
+};
+
 function App() {
-  const input = useRef();
-  setTimeout(() => input.current.focus(), 3000);
+  const sayHello = () => console.log("say hello");
+  const title = useClick(sayHello);
   return (
     <div>
-      <h1>Hi</h1>
-      <input placeholder="good" ref={input}></input>
+      <h1 ref={title}>Hi</h1>
     </div>
   );
 }
