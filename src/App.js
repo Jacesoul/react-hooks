8fcc13aca1be70dc01e0d5ab08f4ad97a2681660
Jacesoul <75877363+Jacesoul@users.noreply.github.com>
2022-02-17 15:46:10 +0900

    useHover

--- src/App.js
+++ src/App.js
@@ -56,14 +56,26 @@ export const useClick = (onClick) => {
   return element;
 };
 
+export const useHover = (onHover) => {
+  if (typeof onHover !== "function") {
+    return;
+  }
+  const element = useRef();
+  useEffect(() => {
+    if (element.current) {
+      element.current.addEventListener("mouseenter", onHover);
+    }
+    return () => {
+      if (element.current) {
+        element.current.removeEventListener("mouseenter", onHover);
+      }
+    };
+  }, []);
+  return element;
+};
+
 function App() {
-  const sayHello = () => console.log("say hello");
-  const title = useClick(sayHello);
-  return (
-    <div>
-      <h1 ref={title}>Hi</h1>
-    </div>
-  );
+  return <div></div>;
 }
 
 export default App;
